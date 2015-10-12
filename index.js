a38c7fec7b9b152bcf12913e8ce893888ff2fd1c
hanhsn <f61901@gmail.com>
2015-10-12 20:09:49 +0800

    javascript

---
+++ index.js
@@ -0,0 +1,21 @@
+function a(){
+        document.getElementById("a").innerHTML = '韓承勳';
+	  }	
+	    function b(){
+	            document.getElementById("b").innerHTML = 'HAN,CHENG-HSUN';
+		      }
+		        function c(){
+			        document.getElementById("c").innerHTML = '男';
+				  }
+				    function d(){
+				            document.getElementById("d").innerHTML = '82/09/29';
+					      }
+					        function e(){
+						        document.getElementById("e").innerHTML = '0988-165332';
+							  }
+							    function f(){
+							            document.getElementById("f").innerHTML = 'f61901@gmail.com';
+								      }
+								        function g(){
+									        document.getElementById("g").innerHTML = '國立成功大學資訊工程學系 (105/6畢)<br>台北市立成功高級中學 (101/6畢)<br>新北市土城區清水國中<br>新北市土城區清水國小';
+										  }
